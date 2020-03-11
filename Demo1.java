e6e8cfe9c11477af72de7a8876692e5acd128bdd
SwapnilSatav <swapnilsatav.k@gmail.com>
2020-03-11 15:27:44 +0530

    Hello World

--- Demo1.java
+++ Demo1.java
@@ -2,6 +2,6 @@ public class Demo {
   public static void main(String[] args) {
     System.out.println("this is demo file");
 	System.out.println("new statement");
-	System.out.println("Hello");
+	System.out.println("Hello World");
   }
 }
